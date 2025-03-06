7e8546a8ec21bf287082c51b90660d24fdd9e71a
Marie-Lois <marieloisajongacham@gmail.com>
2025-03-06 18:11:59 +0100

    added a prompt input password length

--- password_generator.py
+++ password_generator.py
@@ -1,6 +1,7 @@
 import random
 import string
-def generate_password(length=8):
+lengths = int(input("Enter password length: "))
+def generate_password(length = lengths):
     characters = string.ascii_letters + string.digits + string.punctuation
     password = "".join(random.choice(characters) 
     for i in range(length))
